e12896e2814cc5235245e46b131e1555b44a1c13
trasque <taisajazz@gmail.com>
2020-02-01 15:03:10 +0900

    sw.c Initial commit.

---
+++ src/sw.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int num;
+	printf("1-3の値を入れてください：");
+
+	scanf("%d", &num);
+
+	switch (num) {
+	case 1:
+		printf("one\n");
+		break;
+	case 2:
+		printf("two\n");
+		break;
+	case 3:
+		printf("three\n");
+		break;
+	default:
+		printf("不適切な値です。\n");
+		break;
+	}
+
+	return 0;
+}
